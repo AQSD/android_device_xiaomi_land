2542b105f4e78b3c035dcc1230eb35cf23b2d056
Igor Sorocean <igor_sorocean@hotmail.com>
2017-09-22 11:27:47 +0300

    move flp gps izat sap configs to device tree

--- device.mk
+++ device.mk
@@ -156,6 +156,12 @@ PRODUCT_PACKAGES += \
     libgnsspps \
     libcurl
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+
 # Init
 PRODUCT_PACKAGES += \
     init.qcom.fm.sh
